c018a7390e3e4f9f7ec1a737413effa47fa44e8a
JoseHenriqueAbilio <josehenriqueabilio6@gmail.com>
2026-01-15 14:10:25 -0300

    Update enemies-load.js

--- Scripts/enemies-load.js
+++ Scripts/enemies-load.js
@@ -7,10 +7,8 @@ if(localStorage.getItem('filtrar_por') === null){
     localStorage.setItem('filtrar_por', '')
 }
 
-
 let valor_selecionado = localStorage.getItem('filtrar_por')
 filtrar_por.value = valor_selecionado
-
 
 fetch('ArquivosJSON/enemies.json')
     .then(function(response){
@@ -30,58 +28,40 @@ function carregar_inimigo(event){
     inimigo_info_container.innerHTML = lista_inimigos[id].conteudo
 }
 
-
-
-function adicionar_inimigo(x){
+function adicionar_inimigo(indice){
     let caixa = document.createElement('div')
     let enemy_icon = document.createElement('button')
+
     enemy_icon.type = "button"
     caixa.classList.add('shell')
     enemy_icon.classList.add('enemy-icon')
-    enemy_icon.style.backgroundImage = `url(${lista_inimigos[x].icon})`
-    enemy_icon.id = x
+
+    enemy_icon.style.backgroundImage = `url(${lista_inimigos[indice].icon})`
+    enemy_icon.id = indice
     enemy_icon.onclick = carregar_inimigo
+
     inimigos_container.appendChild(caixa)
     caixa.appendChild(enemy_icon)
 }
 
 function listar_inimigos(){
     inimigos_container.innerHTML = ''
-    if(filtrar_por.value === '' || filtrar_por.value === 'Todos'){
-        for(let x = 0; x < lista_inimigos.length;x++){
-            adicionar_inimigo(x)
-        }
+
+    let tipo = filtrar_por.value
+    let inimigos_filtrados
+
+    if(tipo === '' || tipo === 'Todos'){
+        inimigos_filtrados = lista_inimigos
+    } else {
+        inimigos_filtrados = lista_inimigos.filter(function(inimigo){
+            return inimigo.tipodeinimigo === tipo
+        })
     }
-    else if(filtrar_por.value ==='Husk'){
-        for(let x = 0; x < lista_inimigos.length;x++){
-            if(lista_inimigos[x].tipodeinimigo === 'Husk'){adicionar_inimigo(x)}
-        }
-    }
-    else if(filtrar_por.value ==='Demônio'){
-        for(let x = 0; x < lista_inimigos.length;x++){
-            if(lista_inimigos[x].tipodeinimigo === 'Demônio'){adicionar_inimigo(x)}
-        }
-    }
-    else if(filtrar_por.value ==='Máquina'){
-        for(let x = 0; x < lista_inimigos.length;x++){
-            if(lista_inimigos[x].tipodeinimigo === 'Máquina'){adicionar_inimigo(x)}
-        }
-    }
-    else if(filtrar_por.value ==='Anjo'){
-        for(let x = 0; x < lista_inimigos.length;x++){
-            if(lista_inimigos[x].tipodeinimigo === 'Anjo'){adicionar_inimigo(x)}
-        }
-    }
-    else if(filtrar_por.value ==='Boss'){
-        for(let x = 0; x < lista_inimigos.length;x++){
-            if(lista_inimigos[x].tipodeinimigo === 'Boss'){adicionar_inimigo(x)}
-        }
-    }
-    else if(filtrar_por.value ==='Outro'){
-        for(let x = 0; x < lista_inimigos.length;x++){
-            if(lista_inimigos[x].tipodeinimigo === 'Outro'){adicionar_inimigo(x)}
-        }
-    }
+
+    inimigos_filtrados.forEach(function(inimigo){
+        let indice_real = lista_inimigos.indexOf(inimigo)
+        adicionar_inimigo(indice_real)
+    })
 }
 
 function filtragem(){
